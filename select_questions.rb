259d8b8f80c9565cd8be631755488593f49f524f
jamnjon <mendelsonja@gmail.com>
2016-06-08 14:32:37 -0700

    generate random question ids in an array

--- select_questions.rb
+++ select_questions.rb
@@ -3,3 +3,10 @@ until num_questions > 0
   puts "How many questions would you like? (Must be a positive integer)"
   num_questions = gets.chomp.to_i
 end
+
+question_file = File.open("questions.csv")
+questions = question_file.readlines()
+qids = []
+num_questions.times do
+  qids.push(questions[rand(questions.size)].split(",")[4])
+end
