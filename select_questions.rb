1b57f8f640cc5b2edf6daf94964b56e2f3b4da9f
jamnjon <mendelsonja@gmail.com>
2016-06-08 15:23:46 -0700

    equal # for each standard and strand

--- select_questions.rb
+++ select_questions.rb
@@ -9,21 +9,30 @@ questions = question_file.readlines()
 questions = questions.slice(1, questions.length - 1)
 qids = []
 strands = {1 => 0, 2 => 0}
+standards = Array.new(6,0)
 num_questions.times do
   current_size = qids.size
   until qids.size > current_size
     qid = rand(questions.size)
     current_strand = questions[qid][0].to_i
-    # puts "current_strand: #{current_strand}"
-    # puts strands[current_strand]
-    if (strands[1] >= strands[2] && current_strand == 2) ||
-      (strands[1] <= strands[2] && current_strand == 1)
-      puts "inserting #{current_strand}"
+    current_standard = questions[qid].split(',')[2].to_i - 1
+    max, max_idx, min = 0, 0, standards[0]
+    standards.each_with_index do |s, idx|
+      if s > max
+        max_idx = idx
+        max = s
+      end
+      min = s if s < min
+    end
+
+
+    if ((strands[1] >= strands[2] && current_strand == 2) ||
+      (strands[1] <= strands[2] && current_strand == 1)) &&
+      (max == min || standards[current_standard] != standards[max_idx])
+      puts "inserting"
       qids.push(questions[qid].split(",")[4])
       strands[current_strand] += 1
+      standards[current_standard] += 1
     end
   end
 end
-
-puts qids
-puts strands
